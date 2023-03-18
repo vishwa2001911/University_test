dc3024e9df0f254b15031d916f99e69e1b21baca
vishwa <103561819+vishwa2001911@users.noreply.github.com>
2023-03-18 08:47:13 +0530

    Create PythonLabTest03_Q2.py

---
+++ PythonLabTest03_Q2.py
@@ -0,0 +1,33 @@
+"""
+Activity 2
+Create a simple python program where a user can enter the name
+age, address, city and mobile number and display all the details afterwards.
+Ex- If the user enter below details
+     Age – 25
+    Address – 458 Nawala Road Colombo
+    City – Colombo
+    Mobile No – 077777777
+    A message should be displayed saying,
+    “Below are the details you entered:
+    Ex- If the user enter below details
+    Name – Kasun
+    Age – 25
+    Address – 458 Nwala Road Colombo
+    City – Colombo
+    Mobile No – 07777777”’
+"""
+
+name = "V.P Jayasinghe"
+age = 27
+city = "Monaragal"
+Address = "No 27, Kolongolla, Monaragala"
+Mobile_No = "0782548010"
+
+print(f"""
+Name – {name} 
+Age – {age} 
+Address – {Address} 
+City – {city} 
+Mobile No – {Mobile_No}
+    
+""")

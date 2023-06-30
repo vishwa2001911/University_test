311337d4282a39ffc3bdccc4aa6faa3272976ad0
vishwa jayasinghe <103561819+vishwa2001911@users.noreply.github.com>
2023-06-30 22:29:54 +0530

    Create OOP_Practic-LabTest_1.java

---
+++ OOP_Practic-LabTest_1.java
@@ -0,0 +1,68 @@
+/*
+Practice Lab Test
+ Maximum number of files: 1
+ Maximum upload file size: 16 KiB
+Type of work:  Individual work
+
+Instructions 
+Browsing the web and COPYING answers are strictly prohibited. 
+
+Late submissions will receive Zero Marks.
+
+Use proper conventions and indentation when writing the code 
+
+Type your code inside the virtual lab. Don't copy and paste from other places. It will be IDENTIFIED and will result in zero marks.
+
+
+Scenario 
+
+
+A private tutor wants to develop a simple program to automatically grade the student marks for a given subject.
+
+Below criteria is used to grade a mark between 0 – 100.
+
+Mark
+
+Grade
+
+100 >= 75
+
+A
+
+74 >= 65
+
+B
+
+64>= 55
+
+C
+
+55>= 40
+
+D
+
+40>0
+
+F
+
+
+The user should be able to enter student name (s_name) and student mark (s_mark).
+
+Display a relevant messages when taking the user input.
+
+If the entered marks is matched with the above criteria the relevant grade must be calculated.
+
+Finally, the student name (s_name) and the final grade of that student should be displayed as the output.
+
+Else if the mark is not in the range of 0 – 100 a message should be displayed saying “Invalid mark”.
+
+
+Tasks
+
+Develop a simple JAVA program which can execute above functionalities. (When creating identifiers use the names which are given in the brackets.)
+
+Write two assumptions as comments in the code, for the above scenario.
+
+Ex - // This is a comment
+*/
+
